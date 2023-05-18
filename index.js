048c91925cb55930195633bd34dedcf3603607b5
D'Riski Maulana <maulanadriski@gmail.com>
2023-05-19 00:00:42 +0700

    initial commit - starting point

---
+++ index.js
@@ -0,0 +1,63 @@
+/**
+* Programmer: D'Riski Maulana
+* Filename: index.js
+* Contact: driskimaulana@upi.edu
+* Date: May 18, 2023
+* Description: Starting point for this application. Start server.
+* */
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import * as dotenv from 'dotenv';
+
+// swagger configuration
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'BibitUnggulId API',
+      version: '0.1.0',
+      description: 'This is an API for the BibitUnggulId applications',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'BibitUnggulId',
+        url: 'bibitunggul.id',
+        email: 'bibitunggulid@gmail.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js',
+    './src/models/*.js',
+    './src/controllers/*.js'],
+};
+
+const init = () => {
+  // setting up the server
+  const server = express();
+  server.use(bodyParser.json({ limit: '10mb', extended: true }));
+  server.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  server.use(cors());
+
+  //   setting up swagger
+  const specs = swaggerJSDoc(options);
+  server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+  //   get all environment variables from .env
+  dotenv.config();
+  const PORT = process.env.PORT || 5000;
+
+  server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
+};
+
+init();
