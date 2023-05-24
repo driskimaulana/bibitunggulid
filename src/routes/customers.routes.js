c3c0618bb900248dd4de0756bfd66ba6d8653267
D'Riski Maulana <maulanadriski@gmail.com>
2023-05-24 23:20:29 +0700

    customers crud

---
+++ src/routes/customers.routes.js
@@ -0,0 +1,19 @@
+/**
+* Programmer: D'Riski Maulana
+* Filename: customer.routes.js
+* Contact: driskimaulana@upi.edu
+* Date: May 24, 2023
+* Description: This is a routes for functionalities related to customers
+* */
+
+const router = require('express').Router();
+const {
+  getCustomers, getCustomersById, updateCustomers, deleteCustomers,
+} = require('../controllers/customers.controllers');
+
+router.get('/', getCustomers);
+router.get('/:id', getCustomersById);
+router.put('/:id', updateCustomers);
+router.delete('/:id', deleteCustomers);
+
+module.exports = router;
